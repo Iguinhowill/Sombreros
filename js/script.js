6bbe366e72c744e2a867d718dff7a59863aae50c
Igor Florense <73553905+Iguinhowill@users.noreply.github.com>
2022-12-02 03:44:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/*
+document.getElementById("button9").onclick = function() {myFunction9()};
+
+document.getElementById("button1M").onclick = function() {myFunction1M()};
+document.getElementById("button1F").onclick = function() {myFunction1F()};
+
+document.getElementById("button2M").onclick = function() {myFunction2M()};
+document.getElementById("button2F").onclick = function() {myFunction2F()};
+
+document.getElementById("button3M").onclick = function() {myFunction3M()};
+document.getElementById("button3F").onclick = function() {myFunction3F()};
+
+*/
+//separa aq
+
+function showPicture(show1, show2,) {
+  document.getElementById(show1).classList.toggle("show");
+  document.getElementById(show2).classList.toggle("show");
+}
+
+
+function showTeams(show3, show4) {
+  document.getElementById(show3).classList.toggle("apareca");
+  document.getElementById(show4).classList.toggle("apareca");
+
+}
+
+function showGames(show5) {
+  document.getElementById(show5).classList.toggle("apareca1");
+}
+
+
+
+
+
+/*
+
+function myFunction1M() {
+  document.getElementById("jogadores1AnoMasculino1").classList.toggle("show");
+  document.getElementById("jogadores1AnoMasculino2").classList.toggle("show");
+}
+
+function myFunction1F() {
+  document.getElementById("jogadores1AnoMasculino1").classList.toggle("show");
+  document.getElementById("jogadores1AnoMasculino2").classList.toggle("show");
+}
+
+function myFunction2M() {
+  document.getElementById("jogadores2AnoMasculino1").classList.toggle("show");
+  document.getElementById("jogadores2AnoMasculino2").classList.toggle("show");
+}
+
+function myFunction2F() {
+  document.getElementById("jogadores2AnoFeminino1").classList.toggle("show");
+  document.getElementById("jogadores2AnoFeminino2").classList.toggle("show");
+}
+
+function myFunction3M() {
+  document.getElementById("jogadores3AnoMasculino1").classList.toggle("show");
+  document.getElementById("jogadores3AnoMasculino2").classList.toggle("show");
+}
+
+function myFunction3F() {
+  document.getElementById("jogadores3AnoFeminino1").classList.toggle("show");
+  document.getElementById("jogadores3AnoFeminino2").classList.toggle("show");
+}
+
+*/
